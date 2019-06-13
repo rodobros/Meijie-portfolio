e103af362572534ad4d77044fb0a03f4a34b1e64
Rodolphe Brossard <Rodobros@Rodolphes-MacBook-Pro.local>
2019-06-12 23:19:01 -0400

    changed to high quality pictures. added ghost headers

---
+++ src/assets/js/controllers/FrontPageController.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	/*
+	$('main').hide();
+	$('header').hide();
+	$('footer').hide();
+	$('.dont-delete-me').show();
+
+	$("#unlock_button").click(function() {
+	  	if($("#password").val() == "qwerty") {
+			$('main').show();
+			$('header').show();
+			$('footer').show();
+			$('.dont-delete-me').hide();
+		}
+	})
+	*/
+});
+
