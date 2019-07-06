37ff1eb6a35d13d1455a55d1f23b01eee27afdca
Rodolphe Brossard <Rodobros@Rodolphes-MacBook-Pro.local>
2019-07-06 16:26:48 -0400

    case are now responsive. img change. bug fixes

--- src/assets/js/controllers/Case1Controller.js
+++ src/assets/js/controllers/Case1Controller.js
@@ -17,6 +17,23 @@ case1Controller.init = function() {
 	$("#test_prototype_button").click(() => {
 		window.open("https://invis.io/43SR9TVVMK7");
 	})
+
+	$("#other_case2_link").click(() => {
+		router.goToCase2Page();
+	})
+
+	$("#other_case3_link").click(() => {
+		//router.goToCase3Page();
+	})
+
+	$("#other_case4_link").click(() => {
+		//router.goToCase4Page();
+	})
+
+	$("#other_case_daily_link").click(() => {
+		headerController.setDailyUISelected();
+		router.goToDailyUIPage();
+	})
 }
 
 //case1Controller.init();
