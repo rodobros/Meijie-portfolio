ee52ce838ac342430da30180b9585baa18f1cf82
Rodolphe Brossard <Rodobros@rodolphes-mbp.lan>
2021-01-19 21:20:19 -0500

    made changes for v1.1 Earth

--- src/assets/js/controllers/Case1Controller.js
+++ src/assets/js/controllers/Case1Controller.js
@@ -17,7 +17,34 @@ case1Controller.init = function() {
 	$("#test_prototype_button").click(() => {
 		//scroll to bottom
 		$([document.documentElement, document.body]).animate({
-        	scrollTop: $("#section3").offset().top
-    	}, 100);
+			scrollTop: $(".proto_iframe").offset().top
+		}, 100);
 	})
+
+	$(window).resize(function() {
+		case1Controller.resizeStupidIframe();
+	})
+	case1Controller.resizeStupidIframe();
+}
+
+case1Controller.iframeOriginalWidth = 442;
+case1Controller.iframeOriginalHeight = 935;
+case1Controller.bigScreenSideOffset = 300;
+case1Controller.sideMarginsPercentage = 0.13;
+case1Controller.responsivePixelTreshold = 900;
+case1Controller.responsivePixelTresholdMax = 1400;
+
+case1Controller.resizeStupidIframe = function() {
+	var containerSize = (($( window ).width()*(1 - 2 * case1Controller.sideMarginsPercentage)));
+	if ($(window).width() > case1Controller.responsivePixelTreshold) {
+		containerSize = containerSize - case1Controller.bigScreenSideOffset;
+	}
+	if ($(window).width() >= case1Controller.responsivePixelTresholdMax) {
+		containerSize = ((case1Controller.responsivePixelTresholdMax*(1 - 2 * case1Controller.sideMarginsPercentage))) - case1Controller.bigScreenSideOffset;
+	}
+	containerSize = containerSize / 2; // divide by 2 because we have the <ul> next to the iframe which takes half the space
+	var transformRatio = (containerSize / case1Controller.iframeOriginalWidth);
+	$(".proto_iframe").css('transform', 'scale(' + transformRatio + ')');
+	$(".case_ul_1").css('height', Math.floor(case1Controller.iframeOriginalHeight * transformRatio) + 'px');
+	$(".case_ul_2").css('height', Math.floor(case1Controller.iframeOriginalHeight * transformRatio) + 'px');
 }
