cc85770029b61dee55dff05dc11b3b48e7dab58c
Rodolphe Brossard <Rodobros@Rodolphes-MacBook-Pro.local>
2023-07-16 18:55:20 -0400

    Remove non-capitalized files

--- src/assets/js/common.js
+++
@@ -1,9 +0,0 @@
-var common = {}
-
-// function to get an url parameter
-common.urlParam = function(name) {
-	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-  if(results)
-	 return results[1];
-  return 0;
-}
